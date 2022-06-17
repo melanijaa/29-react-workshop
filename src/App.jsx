44cf0b504689ed13aec309782a65766d0906e0d1
Melanija <melanija.sibakovska1@gmail.com>
2022-06-17 11:47:44 +0300

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
-import './App.css';
+/*
+Užduotis su vienu API
+Gauti visus duomenis iš https://pokeapi.co/api/v2/pokemon API ir išrenderinti Pokemons komponente. Nenaudoti bibliotekų. Pvz. https://reactjs.org/docs/faq-ajax.html
+Pokemons komponente turėtų būti panaudotas Pokemon komponentas, kuris per props gauna name ir url iš API.
+*/
+
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+
+    const [pokemon, setPokemon] = useState([]);
+
+    const getData = () => {
+        fetch("https://pokeapi.co/api/v2/pokemon")
+          .then((response) => response.json())
+          .then((data) => {
+            console.log(data.results);
+            setPokemon(data.results);
+          });
+      };
+      getData();
+   
   return (
     <div className="App">
       <header className="App-header">
-      <h1>SECOND REACT WORKSHOP</h1>
-      
+        <h1>SECOND REACT WORKSHOP</h1>
+        <ul>
+          {pokemon.map((name, i) => {
+           <div key={pokemon.name} name={pokemon.name} photo={pokemon.url}></div>;
+          })}
+        </ul>
       </header>
     </div>
   );
